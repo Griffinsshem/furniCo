c773fccc54ae5d7c057bd7f900c4d7987a2d6f5e
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-12 15:46:46 +0300

    fix: correct name error that cause error

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
-export default function Contact() {
+export default function ContactPage() {
   return (
     <main>
       <Navbar />
