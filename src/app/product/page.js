232f20ab1be60b701a2627684018f7de3d6fab6e
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-12 15:41:07 +0300

    feat: add the product page to page.js

---
+++ src/app/product/page.js
@@ -0,0 +1,13 @@
+import Product from "@/components/product";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+export default function ProductPage() {
+  return (
+    <main>
+      <Navbar />
+      <Product />
+      <Footer />
+    </main>
+  );
+}
