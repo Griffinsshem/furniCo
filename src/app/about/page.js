d636f84df2c7fdf651a0d7bb4ad4de388b13e5ba
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-12 15:38:15 +0300

    feat: add about page page.js

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import About from "@/components/about";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Footer />
+    </main>
+  );
+}
