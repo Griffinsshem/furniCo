c90633574012d1b9311115e564753f9f0562d525
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-12 15:37:36 +0300

    feat: update the page.js to  display the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import HomeHero from "@/components/home";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-      <p>Welcome to funico</p>
+      <Navbar />
+      <HomeHero />
+      <Footer />
     </div>
   );
 }
